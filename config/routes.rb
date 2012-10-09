e87751fef69dc9fd5b0ae2917c409a215b5323bb
Nelson Davis <daslen@yahoo.com>
2012-10-09 00:50:39 -0700

    Finish stataic pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ SampleApp::Application.routes.draw do
   get "static_pages/home"
 
   get "static_pages/help"
+  get "static_pages/about"
   #match '/help', to: 'static_pages#help'
   # The priority is based upon order of creation:
   # first created -> highest priority.
